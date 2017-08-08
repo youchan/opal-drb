2be437323bb57bcc5b697f97b962010832fe01e8
youchan <youchan01@gmail.com>
2017-08-08 09:26:36 +0900

    Fix load error when required from MRI

--- lib/opal/drb.rb
+++ lib/opal/drb.rb
@@ -1,15 +1,17 @@
-module DRb
-  class DRbError < RuntimeError; end
-  class DRbConnError < DRbError; end
-  class DRbServerNotFound < DRbError; end
-  class DRbBadURI < DRbError; end
-  class DRbBadScheme < DRbError; end
-end
+if RUBY_ENGINE == 'opal'
+  module DRb
+    class DRbError < RuntimeError; end
+    class DRbConnError < DRbError; end
+    class DRbServerNotFound < DRbError; end
+    class DRbBadURI < DRbError; end
+    class DRbBadScheme < DRbError; end
+  end
 
-require 'opal/drb/version'
-require 'opal/drb/websocket'
-require 'opal/drb/drb_protocol'
-require 'opal/drb/drb_conn'
-require 'opal/drb/drb_object'
-require 'opal/drb/drb_message'
-require 'opal/drb/drb'
+  require 'opal/drb/version'
+  require 'opal/drb/websocket'
+  require 'opal/drb/drb_protocol'
+  require 'opal/drb/drb_conn'
+  require 'opal/drb/drb_object'
+  require 'opal/drb/drb_message'
+  require 'opal/drb/drb'
+end
